dcb08197c8f258707b9ff51453c00196f297da52
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-21 10:58:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index < 4; index++) {
+        sprite = game.createSprite(3, 3)
+        basic.pause(200)
+        sprite.set(LedSpriteProperty.Y, 2)
+    }
 })
+let sprite: game.LedSprite = null
+basic.showIcon(IconNames.Angry)
