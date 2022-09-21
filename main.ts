fbaff50da1cf2a6aeb2f291254fbea3abfc54010
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-21 11:02:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    for (let index = 0; index < 4; index++) {
-        sprite = game.createSprite(3, 3)
+    for (let index = 0; index < 1; index++) {
         basic.pause(200)
         sprite.set(LedSpriteProperty.Y, 2)
+        basic.pause(200)
+        sprite.set(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+        sprite.set(LedSpriteProperty.X, 2)
+        basic.pause(200)
+        sprite.set(LedSpriteProperty.X, 1)
     }
 })
 let sprite: game.LedSprite = null
-basic.showIcon(IconNames.Angry)
+sprite = game.createSprite(3, 3)
